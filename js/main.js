2d1c7612ab1e7b707c53481f94bd41d0de07d583
vznachitvitalik <vitaliy.zherebtcov@gmail.com>
2023-03-01 20:59:44 +0300

    fix icons svg sprite

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const navbar = document.querySelector(".navbar");  /*найти навбар и переместить в переменную navbar в js*/
+const logo = document.querySelector(".logo-svg use");
 window.addEventListener("scroll", ()=>{
   if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light")
+    navbar.classList.add("navbar-light");
+   logo.href.baseVal = "img/sprite.svg#logo";
   } else {
     navbar.classList.remove("navbar-light");
+    logo.href.baseVal = "img/sprite.svg#logo-light";
   }
 });
