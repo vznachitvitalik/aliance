60c5877b5d94407b1e1428220c32b981e40dc383
vznachitvitalik <vitaliy.zherebtcov@gmail.com>
2023-03-07 22:30:24 +0300

    add phone-mail

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const navbar = document.querySelector(".navbar");  /*найти навбар и переместить в переменную navbar в js*/
 const logo = document.querySelector(".logo-svg use");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
+
 window.addEventListener("scroll", ()=>{
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light");
@@ -8,4 +11,8 @@ window.addEventListener("scroll", ()=>{
     navbar.classList.remove("navbar-light");
     logo.href.baseVal = "img/sprite.svg#logo-light";
   }
+});
+mMenuToggle.addEventListener("click", (event)=> {
+  event.preventDefault();
+  menu.classList.toggle("is-open"); /*отслеживание события клик*/
 });
