e6d59802aedfa4e38fa211a24b027076e061eb9d
vznachitvitalik <vitaliy.zherebtcov@gmail.com>
2023-03-08 16:03:14 +0300

    add close menu

--- js/main.js
+++ js/main.js
@@ -3,16 +3,35 @@ const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 
-window.addEventListener("scroll", ()=>{
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light");
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar-light");
    logo.href.baseVal = "img/sprite.svg#logo";
-  } else {
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo-light";
-  }
+};
+
+const lightModeOff = (event) => {
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo-light";
+}
+
+
+const openMenu = (event) => {    /*функция открывания меню */
+  menu.classList.add("is-open");  /*вешает класс is-open */
+  mMenuToogle.classList.add ("close-menu");
+  document.body.style.over="hidden";  // запрещаем прокрутку сайта 
+  lightModeOn ();
+};
+const closeMenu = (event) => {    /*функция закрываания меню */
+  menu.classList.remove("is-open");  /*убирает класс is-open */
+  mMenuToogle.classList.remove ("close-menu");
+  document.body.style.over="";  // возвращает прокрутку сайта 
+  lightModeOff ();
+};
+
+window.addEventListener("scroll", ()=>{
+this.scrollY > 1 ? lightModeOn () : lightModeOff ();
 });
+
 mMenuToggle.addEventListener("click", (event)=> {
   event.preventDefault();
-  menu.classList.toggle("is-open"); /*отслеживание события клик*/
+  menu.classList.contains ("is-open") ? closeMenu () : openMenu ();
 });
