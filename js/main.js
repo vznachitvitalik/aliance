110762e0e1a78fb3fbaec701e539c4b74ab32fc3
vznachitvitalik <vitaliy.zherebtcov@gmail.com>
2023-04-13 22:00:10 +0300

    add modal js

--- js/main.js
+++ js/main.js
@@ -98,16 +98,21 @@ navigation: {
 });
 
 const modal = document.querySelector(".modal");
-const modalToggle = document.querySelectorAll("[data-toggle=modal]");
-const modalClose = document.querySelector(".modal-close");
-console.log(modalToggle);
-modalToggle.forEach((element) => {
-  element.addEventListener("click", (event) => {
+const modalDialog = document.querySelector(".modal-dialog");
+
+document.addEventListener("click", (event) => {
+  if (
+    event.target.dataset.toggle == "modal" || 
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalDialog) &&
+    modal.classList.contains("is-open"))
+  ) {
     event.preventDefault();
-    modal.classList.add("is-open");
-  });
+    modal.classList.toggle('is-open');
+  }
 });
-modalClose.addEventListener("click", (event) => {
-  event.preventDefault();
-  modal.classList.remove ("is-open");
+document.addEventListener("keyup", (event) => {
+ if (event.key == "Escape" && modal.classList.contains("is-open")) {
+  modal.classList.toggle('is-open');
+ }
 });
